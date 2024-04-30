17765614dbb74aeb5b43335d90019117386689ee
and-is <andispaudel@gmail.com>
2024-04-30 12:28:06 +0545

    did the ui for rps

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ let humanScore = 0,
 
 function getComputerChoice() {
   let n = Math.floor(Math.random() * 3);
-  console.log(n);
   let str;
   switch (n) {
     case 0:
-      str = "rock";
+      str = "Rock";
       break;
     case 1:
-      str = "paper";
+      str = "Paper";
       break;
     case 2:
-      str = "scissors";
+      str = "Scissors";
       break;
     default:
       console.log("error");
@@ -23,9 +22,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let n =
-    parseInt();
-    //    prompt("Enter 0,1,2 for rock, paper, scissors respectively")
+  let n = parseInt();
+  prompt("Enter 0,1,2 for rock, paper, scissors respectively");
   let str;
   switch (n) {
     case 0:
@@ -45,33 +43,77 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  console.log("you chose " + humanChoice);
-  console.log("computer chose " + computerChoice);
+  if (humanScore === 5) {
+    f.innerText = "";
+    sc.innerText = "";
+    humanScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    f.innerText = "";
+    sc.innerText = "";
+    computerScore = 0;
+    humanScore = 0;
+  }
+  hum.innerText = "You chose " + humanChoice;
+  comp.innerText = "Computer chose " + computerChoice;
   if (
-    (humanChoice == "rock" && computerChoice == "paper") ||
-    (humanChoice == "paper" && computerChoice == "scissors") ||
-    (humanChoice == "scissors" && computerChoice == "rock")
+    (humanChoice == "Rock" && computerChoice == "Paper") ||
+    (humanChoice == "Paper" && computerChoice == "Scissors") ||
+    (humanChoice == "Scissors" && computerChoice == "Rock")
   ) {
-    console.log("You lose!");
+    wl.innerText = "You lose!";
     computerScore++;
   } else if (
-    (humanChoice == "rock" && computerChoice == "rock") ||
-    (humanChoice == "paper" && computerChoice == "paper") ||
-    (humanChoice == "scissors" && computerChoice == "scissors")
+    (humanChoice == "Rock" && computerChoice == "Rock") ||
+    (humanChoice == "Paper" && computerChoice == "Paper") ||
+    (humanChoice == "Scissors" && computerChoice == "Scissors")
   ) {
-    console.log("Draw!");
+    wl.innerText = "Draw!";
   } else if (
-    (humanChoice == "paper" && computerChoice == "rock") ||
-    (humanChoice == "scissors" && computerChoice == "paper") ||
-    (humanChoice == "rock" && computerChoice == "scissors")
+    (humanChoice == "Paper" && computerChoice == "Rock") ||
+    (humanChoice == "Scissors" && computerChoice == "Paper") ||
+    (humanChoice == "Rock" && computerChoice == "Scissors")
   ) {
-    console.log("You won!");
+    wl.innerText = "You win!";
     humanScore++;
   } else {
-    console.log("Invalid input");
+    wl.innerText = "Invalid Input!";
   }
-  console.log("scores are human:" + humanScore + " computer: " + computerScore);
+  scrs.innerText =
+    "Scores are \n You : " + humanScore + " \n Computer : " + computerScore;
+  if (humanScore === 5) {
+    console.log("'tis time");
+    f.innerText = "You're the champ";
+    sc.innerText =
+      "Final Scores : \n You: " +
+      humanScore +
+      "\nVS\n  Computer: " +
+      computerScore;
+  } else if (computerScore === 5) {
+    console.log("'tis time");
+    f.innerText = "Tough luck mate, try again?";
+    sc.innerText =
+      "Final Scores : \n You: " +
+      humanScore +
+      "\nVS\n  Computer: " +
+      computerScore;
+  }
 }
+
+const hum = document.querySelector(".humanChoice");
+const comp = document.querySelector(".compChoice");
+const wl = document.querySelector(".winOrNo");
+const scrs = document.querySelector(".scores");
+
+const f = document.querySelector(".fin");
+const sc = document.querySelector(".finScores");
+
+const butt = document.querySelectorAll(".btn");
+butt.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.innerText, getComputerChoice());
+  });
+});
 
 function playGame() {
   let humanChoice;
@@ -89,5 +131,3 @@ function playGame() {
     console.log("Tough luck mate, try again?");
   }
 }
-
-playGame();
